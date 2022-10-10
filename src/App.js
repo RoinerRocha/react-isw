10ca890e9cf6441bb12766ec4f6415d6ef6d8883
RoinerRocha <rochabozaroiner@gmail.com>
2022-10-09 20:21:56 -0600

    WorkShop2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/header/Header';
 import Body from './components/body/Body';
 import Footer from './components/footer/Footer';
+import Modal from './components/Modal/modal';
 
 function App() {
   return (
     <div className="container mt-2">
       <Header text={'My first react App'}/>
       <Body />
+      <Modal />
       <Footer />
     </div>   
   );
